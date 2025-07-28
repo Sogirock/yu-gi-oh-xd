c0d3c205ee6ed7b04b70549a498b51cd5dad61d4
sogirock <gregorybrehm6@gmail.com>
2025-07-29 00:50:28 +0200

    message

---
+++ data/cards.js
@@ -0,0 +1,28 @@
+// data/cards.js
+
+export const cards = [
+  {
+    id: 1,
+    nom: "lutin sauvage",
+    type: "monstre",
+    atk: 1300,
+    def: 1400,
+    effet: "Petit démon joueur qui se tapit dans la pénombre, attendant d'attaquer un ennemi imprudent",
+    image: "https://s2.qwant.com/thumbr/474x697/c/5/ca3875712810f6d85161df13fa1a97eb3d768908fd2ed9f3ad2104f85af53a/OIP.kDEdVaFQvCF_OXzbF2WvcQHaK5.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%2Fid%2FOIP.kDEdVaFQvCF_OXzbF2WvcQHaK5%3Fr%3D0%26pid%3DApi&q=0&b=1&p=0&a=0"
+  },
+  {
+    id: 2,
+    nom: "Corne de la Licorne",
+    type: "magie",
+    effet: "Le monstre équipé gagne 700 ATK/DEF.",
+    image: "https://static.wikia.nocookie.net/yugioh-gx/images/3/3f/CornedelaLicorne-YGLD-FR-C-1E.png/revision/latest/scale-to-width-down/300?cb=20180730161957&path-prefix=fr"
+  },
+  {
+    id: 3,
+    nom: "trappe",
+    type: "piège",
+    effet: "Détruit un monstre attaquant.",
+    image: "https://s2.qwant.com/thumbr/0x0/6/d/f5a3ee2433a26abf98bd63f06d05dc2b9e88765688aeabbc6bcefad40c6801/th.jpg?u=https%3A%2F%2Fwww.bing.com%2Fth%3Fid%3DOIP.BvEScvdyDBzh9IpA5mnIPQAAAA%26w%3D160%26h%3D160%26c%3D7%26pid%3D5.1&q=0&b=1&p=0&a=0"
+  },
+  // Ajoute d'autres cartes ici (tu peux en faire jusqu’à 20+)
+];
