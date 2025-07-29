c7fd648d28b56c4b513ac70d23732d0dc8e39589
Sogirock <gregorybrehm6@gmail.com>
2025-07-29 03:37:54 +0200

    Update cards.js

---
+++ data/cards.js
@@ -0,0 +1,25 @@
+export const cards = [
+  {
+    id: 1,
+    nom: "Lutin Sauvage",
+    type: "monstre",
+    atk: 1300,
+    def: 1400,
+    effet: "Un monstre agile et rusé.",
+    image: "https://s2.qwant.com/thumbr/474x474/d/0/1c21261e9214847341d9fc8caa7d285bc34a1c5aee95f5aee372a07131b883/OIP.klwWxR0MLhvTz95zubAwWAHaHa.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%2Fid%2FOIP.klwWxR0MLhvTz95zubAwWAHaHa%3Fpid%3DApi&q=0&b=1&p=0&a=0"
+  },
+  {
+    id: 2,
+    nom: "Corne de Licorne",
+    type: "magie",
+    effet: "Augmente l'attaque de tous les monstres de 500 points.",
+    image: "https://s2.qwant.com/thumbr/474x699/c/d/e836d145a8b3b025ac563efad22ee6da184993f732bec4a193ffb53b17801a/OIP.ys97zbciGGDBW3TT1NyIYQHaK7.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%2Fid%2FOIP.ys97zbciGGDBW3TT1NyIYQHaK7%3Fr%3D0%26pid%3DApi&q=0&b=1&p=0&a=0"
+  },
+  {
+    id: 3,
+    nom: "Piège Trappe",
+    type: "piège",
+    effet: "detruit 1 monstre de l'adversaire",
+    image: "https://s2.qwant.com/thumbr/0x0/6/d/f5a3ee2433a26abf98bd63f06d05dc2b9e88765688aeabbc6bcefad40c6801/th.jpg?u=https%3A%2F%2Fwww.bing.com%2Fth%3Fid%3DOIP.BvEScvdyDBzh9IpA5mnIPQAAAA%26w%3D160%26h%3D160%26c%3D7%26pid%3D5.1&q=0&b=1&p=0&a=0"
+  }
+];
